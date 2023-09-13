0934e0f201eb3e623d511e911e772d6207d932ca
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-13 16:06:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let isPresent = false;
+	
+	for(let i in sampleObject){
+		if(key == i){
+			isPresent = true;
+		}
+			
+	}
+	return isPresent;
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
